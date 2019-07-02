b9573b8f88041eca095ccf393ff7a87f1ab91197
KhanSuhaib23 <suhaib.niazul.khan@gmail.com>
2019-07-02 18:06:20 +0530

    Lecture 22 2/2

--- lect22/code/diff.js
+++ lect22/code/diff.js
@@ -1,6 +1,9 @@
-let arr1 = [1, 2, 3, 4, 5]
-let arr2 = [1, 3, 4, 6]
+// let arr1 = [1, 2, 3, 4, 5]
+// let arr2 = [1, 3, 4, 6]
 
-let diff = arr1.filter(ele1 => !arr2.some( ele2 => ele1 === ele2 ))
+// let diff = arr1.filter(ele1 => !arr2.some( ele2 => ele1 === ele2 ))
 
-console.log(diff)
+// console.log(diff)
+
+
+let symbol = Symbol.keyFor('ajax');
